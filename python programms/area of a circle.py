ac1b531ba3fb77661d25544b354844e9527c7722
JagadeeshMandala <70205006+JagadeeshMandala@users.noreply.github.com>
2020-11-28 18:01:55 +0530

    second commit

--- python programms/area of a circle.py
+++ python programms/area of a circle.py
@@ -2,5 +2,7 @@ import math
 def area_of_a_circle (r):
     res=math.pi*r*r
     return res
+
+    
 r=float(input("enter the radius of the circle")) 
 print("The radius of the circle is",area_of_a_circle(4))   
